a952ce8fe58bb79bb533bb6f9117b689231aaca6
kailanesarah <kailanesarahpro@gmail.com>
2024-09-18 11:10:04 -0300

    versao_atualizada

--- venv/cars/admin.py
+++ venv/cars/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from cars.models import Car, Brand
 
-# Register your models here.
+class CarBrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)  # Corrigido para pesquisar apenas pelo campo 'name'
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'model', 'brand', 'factory_year', 'model_year', 'value', 'photo')
+    search_fields = ('model', 'brand__name')  # Adicionando a pesquisa por nome da marca se necessário
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, CarBrandAdmin)
--- venv/cars/models.py
+++ venv/cars/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
-class cars(models.Model):
-    id = models.AutoField(primary_key= True)
-    model = models.CharField(max_length=200, null=True)
-    brand = models.CharField(max_length=200, null=True)
-    factory_year = models.IntegerField(null=True)
-    model_year = models.IntegerField(null = True)
-    value = models.FloatField(null=True)
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, blank=True)  # Utilizando blank=True em vez de null=True
+    
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200, blank=True)  # Utilizando blank=True em vez de null=True
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='cars')  
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)  # Permitir campos em branco
+    
+    def __str__(self):
+        return self.model
